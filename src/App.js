ab09eb7709a8133c79613a11ea3019899564a082
IamAbhi12 <abhinav.choudhary@razorpay.com>
2024-06-02 08:42:00 +0530

    fixed body margin issue

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Workspaces from "./Pages/Workspaces";
 import AboutUs from "./Pages/AboutUs";
+import "./reset.css";
 
 function App() {
   return (
